340b08b6e9eb983dd08454a074e86f7857a2c9c3
Violeta Gonzalez-Perez <violetagp@protonmail.com>
2020-10-23 13:02:27 +0200

    Added path to files

--- lo2_cum.py
+++ lo2_cum.py
@@ -1,2 +1,18 @@
-unitdir = '/data6/users/aknebe/Projects/UNITSIM/'
-samdir = '/home/aknebe/Projects/UNITSIM/SAMs/SAGE/ELGs/new/'
+
+zz = 0.987
+
+sims = ['UNITSIM1','UNITSIM1_InvPhase','UNITSIM2','UNITSIM2_InvPhase']
+#sims = ['UNITSIM1']
+
+unitdir = '/data6/users/aknebe/Projects/UNITSIM/ELGs/'
+
+#############################
+#line = 'OII3727' ; lline = '[OII]'
+#outdir = '/cosma5/data/durham/violeta/lines/cosmicweb/plots/'+model+'selections/lo2_cum_'
+#plotfile = outdir+line+'.pdf'
+#############################
+
+for sim in sims:
+    ff = unitdir+sim+'/'+sim+'_model_z'+str(zz)+'_ELGs.h5'
+    print(ff)
+
