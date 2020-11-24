4d0e6528fa044496e93ace648113ca26968c3c45
Violeta Gonzalez-Perez <violetagp@protonmail.com>
2020-11-24 11:01:04 +0100

    Writing ascii file for selection done

--- create_sel.py
+++ create_sel.py
@@ -5,7 +5,9 @@ import numpy as np
 from Cosmology import set_cosmology,H
 from iotools import check_file
 
-Testing = True
+Testing = False
+
+h5file = False
 
 zz = 0.987
 
@@ -31,6 +33,14 @@ set_cosmology(omega0=0.3089,omegab=0.02234/h0/h0,h0=h0,
 Hz = H(zz)
 
 for sim in sims:   
+    # Prep output directories
+    if h5file:
+        outpath = outdir+sim+'/h5_files/'
+    else:
+        outpath = outdir+sim+'/ascii_files/'
+    if not os.path.exists(outpath):
+        os.makedirs(outpath)
+        
     # Read the data from the sim
     ff = unitdir+sim+'/'+sim+'_model_z'+str(zz)+'_ELGs.h5'
     if (not check_file(ff)):  continue
@@ -47,17 +57,28 @@ for sim in sims:
     xgalz = f['Xpos'][ifirst:ilast] + f['Xvel'][ifirst:ilast]*(1+zz)/Hz
     ygalz = f['Ypos'][ifirst:ilast] + f['Yvel'][ifirst:ilast]*(1+zz)/Hz
     zgalz = f['Zpos'][ifirst:ilast] + f['Zvel'][ifirst:ilast]*(1+zz)/Hz
-
+    mass = f['Mstar'][ifirst:ilast] # Msun/h
+    sfr = f['SFR'][ifirst:ilast]*10**9 # Msun/h/Gyr
+    lo2 = 10**f['logLOII_3727_att'][ifirst:ilast] +\
+          10**f['logLOII_3729_att'][ifirst:ilast]
+    
     for ip,prop in enumerate(props):
-        vals = np.fill(shape=len(mhalo),-999.)
-        ind = np.where(mhalo>0)
+        vals = np.full(len(mhalo),-999.)
         if (prop == 'mass'): #here vals in adequate units
-            vals[ind] = f['Mstar'][ind] # Msun/h
+            array = mass
+            ind = np.where((mhalo>0) & (array>0))
+            vals[ind] = np.log10(array[ind])
+            array = []
         elif (prop =='sfr'):
-            vals[ind] = f['SFR'][ind]*10**9 # Msun/h/Gyr
+            array = sfr
+            ind = np.where((mhalo>0) & (array>0))
+            vals[ind] = np.log10(array[ind])
+            array = []
         elif (prop == 'lo2'):
-            vals[ind] = 10**f['logLOII_3727_att'][ind] +\
-                        10**f['logLOII_3729_att'][ind]
+            array = lo2
+            ind = np.where((mhalo>0) & (array>0))
+            vals[ind] = np.log10(array[ind]) + 2*np.log10(h0) # erg/s/h**2 
+            array = []
 
         # Read the prop cuts
         ndfile = outdir+sim+'/ngal_'+prop+'_cuts_z'+redshift+'.dat'
@@ -65,159 +86,47 @@ for sim in sims:
         if np.isscalar(nds):                                                                          
             nds = np.array([nds]) ; cuts = np.array([cuts])   
 
-        for nd in nds:
-            ind = np.where(vals > n)
-#            
-#            gfile = path+model+'iz'+sn+'/ivol'+str(ivol)+'/galaxies.hdf5'
-#        if (os.path.isfile(gfile)):        
-#            # Get some of the model constants
-#            f = h5py.File(gfile,'r')
-#            group = f['Parameters']
-#            vol1 = group['volume'].value ; volume = volume + vol1
-#            h0 = group['h0'].value ; lambda0 =group['lambda0'].value
-#            omega0 = group['omega0'].value ; omegab = group['omegab'].value
-#    
-#            group = f['Output001']
-#            zz     = group['redshift'].value
-#            tomag = band_corrected_distance_modulus(zz)
-#    
-#            xgal   = group['xgal'].value   # Mpc/h
-#            ygal   = group['ygal'].value
-#            zgal   = group['zgal'].value
-#            vxgal  = group['vxgal'].value*(1.+zz)/H(zz)  # km/s
-#            vygal  = group['vygal'].value*(1.+zz)/H(zz)
-#            vzgal  = group['vzgal'].value*(1.+zz)/H(zz)
-#    
-#            mhhalo = group['mhhalo'].value   # Msun/h
-#            gtype  = group['type'].value # 0= Centrals; 1,2= Satellites
-#    
-#            mdisk = group['mstars_disk'].value # Msun/h
-#            mbulge = group['mstars_bulge'].value
-#            mass1 = mdisk + mbulge
-#
-#            sdisk = group['mstardot'].value # Msolar/h/Gyr
-#            sbulge = group['mstardot_burst'].value
-#            sfr1 = sdisk + sbulge
-#    
-#            f.close()
-#    
-#            gfile = path+model+'iz'+sn+'/ivol'+str(ivol)+'/elgs.hdf5'
-#            if (not os.path.isfile(gfile)):        
-#                print('STOP {} not found'.format(gfile)) ; sys.exit()
-#            f = h5py.File(gfile,'r')
-#            group = f['Output001']
-#    
-#            lum = group['L_tot_'+line].value # 10^40 h^-2 erg/s
-#            lum_ext = group['L_tot_'+line+'_ext'].value 
-#    
-#
-#            for survey in surveys:
-#                for nd in nds:
-#                    # Find the mass cut
-#                    ind=np.where((nds_all == nd) & (ndsurveys == survey))
-#                    if(np.shape(ind)[1]==1):
-#                        cut = cuts[ind]
-#                    else:
-#                        print('STOP: More or none one cut value, index_shape= {}, sn={}, survey={}, ns={}'.format(np.shape(ind)[1],sn,survey,nd)) ; sys.exit()
-#
-#                    if (cut<0.): continue
-#
-#                    if (survey == 'DEEP2'):
-#                        fluxcut = 2.7*10.**-17
-#                        mcut = 24.1
-#                        band = 'DEIMOS-R'
-#                        
-#                        mag = group['mag_'+band+'_o_tot_ext'].value + tomag
-#                        sel0 = (mag < mcut)
-#                        
-#                    elif (survey == 'VVDS-DEEP'):
-#                        fluxcut = 1.9*10.**-17.
-#                        mcut = 24.
-#                        band = 'MegaCam-i-atmos'
-#                        
-#                        mag = group['mag_'+band+'_o_tot_ext'].value + tomag
-#                        sel0 = (mag <= mcut)
-#                        
-#                    elif (survey == 'VVDS-WIDE'):
-#                        fluxcut = 3.5*10.**-17.
-#                        mcut = 22.5
-#                        band = 'MegaCam-i-atmos'
-#                        
-#                        mag = group['mag_'+band+'_o_tot_ext'].value + tomag
-#                        sel0 = (mag <= mcut)
-#                        
-#                    elif (survey == 'eBOSS-SGC'): 
-#                        fluxcut = 10.**-16. #erg/s/cm^2
-#
-#                        g = group['mag_DES-g_o_tot_ext'].value + tomag 
-#                        r = group['mag_DES-r_o_tot_ext'].value + tomag 
-#                        z = group['mag_DES-z_o_tot_ext'].value + tomag 
-#                        rz = r-z ; gr = g-r
-#                        
-#                        sel0 = (g>21.825) & (g<22.825) & \
-#                            (gr>-0.068*rz + 0.457) & \
-#                            (gr< 0.112*rz + 0.773) & \
-#                            (rz> 0.218*gr + 0.571) & \
-#                            (rz<-0.555*gr + 1.901)
-#                        
-#                    elif (survey == 'DESI'):
-#                        fluxcut = 8.*10.**-17. #erg/s/cm^2
-#                        
-#                        g = group['mag_DES-g_o_tot_ext'].value + tomag 
-#                        r = group['mag_DES-r_o_tot_ext'].value + tomag 
-#                        z = group['mag_DES-z_o_tot_ext'].value + tomag 
-#                        rz = r-z ; gr = g-r
-#                        
-#                        sel0 = (r<23.4) & (rz>0.3) & (gr>-0.3) & \
-#                               (gr<1.1*rz-0.13) & (gr<1.6-1.18*rz)
-#
-#                    if (survey == 'All'):
-#                        ind = np.where((mass1>10**cut) &
-#                                       (mhhalo>0.) & (sfr1>0.) )
-#                    else:
-#                        lcut = emission_line_luminosity(fluxcut,zz)
-#                        ind = np.where((mass1>10**cut) &
-#                                       (mhhalo>0.) & (sfr1>0.) &
-#                                       sel0 & (lum_ext>lcut))
-#
-#                    if (np.shape(ind)[1]<1): continue
-#
-#                    massh = np.log10(mhhalo[ind])
-#                    mass = np.log10(mass1[ind])
-#                    sfr = np.log10(sfr1[ind])
-#
-#                    tofile = np.column_stack((xgal[ind],\
-#                                              ygal[ind],\
-#                                              zgal[ind],\
-#                                              vxgal[ind],\
-#                                              vygal[ind],\
-#                                              vzgal[ind],\
-#                                              massh,mass,sfr,\
-#                                              lum[ind],lum_ext[ind], gtype[ind]))
-#
-#                    outm = ndpath+model+'ascii_files/mcut_'+\
-#                           survey+'_nd'+str(nd)+'_sn'+sn+'.dat'
-#    
-#                    with open(outm,'a') as outf:
-#                        np.savetxt(outf, tofile, fmt ='%.5f')
-#    
-#            f.close()
-#
-#lbox = pow(volume,1./3.)
-#print(zz,'Box side (Mpc/h) =',lbox)
-##            outf.write('# xgal,ygal,zgal (Mpc/h), vxgal,vygal,vzgal (Km/s), log10(massh),log10(mass/Msun/h), log10(sfr/Msun/h/Gyr), lum,lum_ext (10^40 h^-2 erg/s), type (0= Centrals; 1,2= Satellites) \n')
-#        #####################
-#        for nd in nds:
-#            snd = str("{:.2f}".format(abs(nd))).replace('.','_')
-#            # Generate output files with a header
-#            outm = outdir+sim+'/'+sim+'_'+prop+'_nd'+snd+'_z'+redshift+'.hdf5'
-#            hf = h5py.File(outm, 'w')
-#            head = hf.create_dataset('header',(100,))
-#            head.attrs[u'Simulation']    = sim
-#            head.attrs[u'h0']            = h0
-#            head.attrs[u'redshift']      = zz
-#            head.attrs[u'number_density']= nd
-#            head.attrs[u'units_nd']      = u'log10(nd/(Mpc/h)^-3)'
-#            head.attrs[u'sel_property']  = prop
-#            print('Output: {}'.format(outm)) #; print(list(head.attrs.items()))      
-#        #####################
+        for ic,cut in enumerate(cuts):
+            ind = np.where(vals > cut)
+            if(np.shape(ind)[1]<1): continue
+
+            nd = str("{:.2f}".format(abs(nds[ic]))).replace('.','_')
+            if not h5file:
+                outm = outpath+prop+'_cuts_nd'+nd+'_z'+redshift+'.dat'
+                tofile = np.column_stack((xgal[ind],ygal[ind],zgal[ind],zgalz[ind]))
+                with open(outm,'w') as outf:
+                    np.savetxt(outf, tofile, fmt ='%.10e')
+                    
+            if h5file:
+                outm = outpath+prop+'_cuts_nd'+nd+'_z'+redshift+'.h5'
+                hf = h5py.File(outm, 'w')
+
+                # Header
+                head = hf.create_group('header')
+                head.attrs[u'Simulation']    = sim
+                head.attrs[u'h0']            = h0
+                head.attrs[u'redshift']      = zz
+                head.attrs[u'number_density']= nd
+                head.attrs[u'units_nd']      = u'log10(nd/(Mpc/h)^-3)'
+                head.attrs[u'sel_property']  = prop
+                head.attrs[u'units_data']    = u'xgal,ygal,zgal, xgalz, ygalz, zgalz (Mpc/h), log10(mass/Msun/h), log10(sfr/Msun/h/Gyr), log10(lum/h^-2 erg/s), sat(1 sat, 0 cen)'
+                print(list(head.attrs.items())) ; sys.exit()
+
+                # Data
+                data = hf.create_group('data')
+                data.create_dataset('xgal',data=xgal[ind])
+                data.create_dataset('ygal',data=ygal[ind])
+                data.create_dataset('zgal',data=zgal[ind])
+                data.create_dataset('xgalz',data=xgalz[ind])
+                data.create_dataset('ygalz',data=ygalz[ind])
+                data.create_dataset('zgalz',data=zgalz[ind])
+                data.create_dataset('log10mhalo',data=np.log10(mhalo[ind]))
+                data.create_dataset('log10mass',data=np.log10(mass[ind]))
+                data.create_dataset('log10sfr',data=np.log10(sfr[ind]))
+                data.create_dataset('log10lo2_att',data=np.log10(lo2[ind]) + 2*np.log10(h0))
+                hf.close()
+            print('Output: {}'.format(outm))
+    
+    f.close()
+
+
